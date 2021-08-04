84bf988723f2d1d9fb63c3fd01f20b6e532fb35c
Paneeshbs77 <paneeshbs528@gmail.com>
2021-08-04 17:22:57 +0530

    Update VehicleEntity.java

--- src/main/java/com/cg/aps/entities/VehicleEntity.java
+++ src/main/java/com/cg/aps/entities/VehicleEntity.java
@@ -1,5 +1,6 @@
 package com.cg.aps.entities;
 
+import java.sql.Timestamp;
 import java.util.Date;
 
 public class VehicleEntity extends BaseEntity {
@@ -13,6 +14,20 @@ public class VehicleEntity extends BaseEntity {
 	private String vehicleType;
 
 	
+	
+	public VehicleEntity(long id, String createdBy, String modifiedBy, Timestamp createdDateTime,
+			Timestamp modifiedDateTime, String name, String parkingNo, String arrivalTime, String departureTime,
+			Date date, String vehicleNo, String vehicleType) {
+		super(id, createdBy, modifiedBy, createdDateTime, modifiedDateTime);
+		this.name = name;
+		this.parkingNo = parkingNo;
+		this.arrivalTime = arrivalTime;
+		this.departureTime = departureTime;
+		this.date = date;
+		this.vehicleNo = vehicleNo;
+		this.vehicleType = vehicleType;
+	}
+
 	public String getKey() {
 		// TODO Auto-generated method stub
 		return null;
