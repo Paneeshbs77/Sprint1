182657b74544fb54e43325f41a39ee34ab8067ee
Paneeshbs77 <paneeshbs528@gmail.com>
2021-08-04 17:11:34 +0530

    Update

--- src/main/java/com/cg/aps/entities/BaseEntity.java
+++ src/main/java/com/cg/aps/entities/BaseEntity.java
@@ -2,12 +2,59 @@ package com.cg.aps.entities;
 
 import java.sql.Timestamp;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public abstract class BaseEntity implements DropdownList {
 
-	protected long id;
-	protected String createdBy;
-	protected String modifiedBy;
-	protected Timestamp createdDateTime;
-	protected Timestamp modifiedDateTime;
+	@Id
+	private long id;
+	private String createdBy;
+	private String modifiedBy;
+	private Timestamp createdDateTime;
+	private Timestamp modifiedDateTime;
+	public BaseEntity(long id, String createdBy, String modifiedBy, Timestamp createdDateTime,
+			Timestamp modifiedDateTime) {
+		super();
+		this.id = id;
+		this.createdBy = createdBy;
+		this.modifiedBy = modifiedBy;
+		this.createdDateTime = createdDateTime;
+		this.modifiedDateTime = modifiedDateTime;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	public String getModifiedBy() {
+		return modifiedBy;
+	}
+	public void setModifiedBy(String modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+	public Timestamp getCreatedDateTime() {
+		return createdDateTime;
+	}
+	public void setCreatedDateTime(Timestamp createdDateTime) {
+		this.createdDateTime = createdDateTime;
+	}
+	public Timestamp getModifiedDateTime() {
+		return modifiedDateTime;
+	}
+	public void setModifiedDateTime(Timestamp modifiedDateTime) {
+		this.modifiedDateTime = modifiedDateTime;
+	}
+	
+	
 
+	
 }
