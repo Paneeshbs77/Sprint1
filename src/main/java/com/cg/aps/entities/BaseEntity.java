eae1184f3c18b26b58830bbf0101449cbbf6fa01
Paneeshbs77 <paneeshbs528@gmail.com>
2021-08-04 17:30:17 +0530

    Update BaseEntity.java

--- src/main/java/com/cg/aps/entities/BaseEntity.java
+++ src/main/java/com/cg/aps/entities/BaseEntity.java
@@ -9,52 +9,11 @@ import javax.persistence.Id;
 public abstract class BaseEntity implements DropdownList {
 
 	@Id
-	private long id;
-	private String createdBy;
-	private String modifiedBy;
-	private Timestamp createdDateTime;
-	private Timestamp modifiedDateTime;
-	public BaseEntity(long id, String createdBy, String modifiedBy, Timestamp createdDateTime,
-			Timestamp modifiedDateTime) {
-		super();
-		this.id = id;
-		this.createdBy = createdBy;
-		this.modifiedBy = modifiedBy;
-		this.createdDateTime = createdDateTime;
-		this.modifiedDateTime = modifiedDateTime;
-	}
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getCreatedBy() {
-		return createdBy;
-	}
-	public void setCreatedBy(String createdBy) {
-		this.createdBy = createdBy;
-	}
-	public String getModifiedBy() {
-		return modifiedBy;
-	}
-	public void setModifiedBy(String modifiedBy) {
-		this.modifiedBy = modifiedBy;
-	}
-	public Timestamp getCreatedDateTime() {
-		return createdDateTime;
-	}
-	public void setCreatedDateTime(Timestamp createdDateTime) {
-		this.createdDateTime = createdDateTime;
-	}
-	public Timestamp getModifiedDateTime() {
-		return modifiedDateTime;
-	}
-	public void setModifiedDateTime(Timestamp modifiedDateTime) {
-		this.modifiedDateTime = modifiedDateTime;
-	}
+	protected long id;
+	protected String createdBy;
+	protected String modifiedBy;
+	protected Timestamp createdDateTime;
+	protected Timestamp modifiedDateTime;
 	
-	
-
 	
 }
